c1113d430a51f2b6901987a97236ca103ecb391a
tubianca <tugbaasik@live.com>
2023-01-07 21:43:13 +0300

    Update lava.sh

--- Lava/lava.sh
+++ Lava/lava.sh
@@ -1,44 +1,37 @@
 #!/bin/bash
 
-WILL BE UPDATE.. DONT USE THIS SCRIPT
-echo  "
+GREEN="\e[32m"
+LIGHT_GREEN="\e[92m"
+YELLOW="\e[33m"
+DEFAULT="\e[39m"
 
-████████╗██╗   ██╗██████╗ ██╗ █████╗ ███╗   ██╗ ██████╗ █████╗  
-╚══██╔══╝██║   ██║██╔══██╗██║██╔══██╗████╗  ██║██╔════╝██╔══██╗
-   ██║   ██║   ██║██████╔╝██║███████║██╔██╗ ██║██║     ███████║
-   ██║   ██║   ██║██╔══██╗██║██╔══██║██║╚██╗██║██║     ██╔══██║
-   ██║   ╚██████╔╝██████╔╝██║██║  ██║██║ ╚████║╚██████╗██║  ██║
-   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚═╝  ╚═╝
- "
-
-
-                                                                                    
-                                                                                        
-sleep 3
-
-# set vars
-if [ ! $NODENAME ]; then
-read -p "Enter node name: " NODENAME
-echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
-fi
+function install_node {
+   echo -e "\e[1m\e[32m	Enter your Node Name:\e[0m"
+   read MONIKER
+   echo export MONIKER=${MONIKER} >> $HOME/.bash_profile
+   source ~/.bash_profile
 
 
-sudo apt update 
+    echo "Installing Depencies..."
+    sudo apt update 
+    sudo apt upgrade -y
 sudo apt install -y unzip logrotate git jq sed wget curl coreutils systemd
 temp_folder=$(mktemp -d) && cd $temp_folder
-
-# Configurations
-go_package_url="https://go.dev/dl/go1.18.linux-amd64.tar.gz"
-go_package_file_name=${go_package_url##*\/}
-wget -q $go_package_url
-sudo tar -C /usr/local -xzf $go_package_file_name
-echo "export PATH=\$PATH:/usr/local/go/bin" >>~/.profile
-echo "export PATH=\$PATH:\$(go env GOPATH)/bin" >>~/.profile
-source ~/.profile
-cd
-
-### Download Lava
-git clone https://github.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T.git
+    
+    echo "Installing GO"
+   if ! [ -x "$(command -v go)" ]; then
+     ver="1.19.4"
+     cd $HOME
+     wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+     sudo rm -rf /usr/local/go
+     sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+     rm "go$ver.linux-amd64.tar.gz"
+     echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+     source ~/.bash_profile
+   fi
+    
+    echo "Downloading and building binaries..."
+    git clone https://github.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T.git
 cd GHFkqmTzpdNLDd6T/testnet-1
 source setup_config/setup_config.sh
 echo "Lava config file path: $lava_config_folder"
@@ -47,14 +40,14 @@ mkdir -p $lava_config_folder
 cp default_lavad_config_files/* $lava_config_folder
 cp genesis_json/genesis.json $lava_config_folder/genesis.json
 
-### Download Lava
+    echo "Install and building Cosmovisor..."
 go install github.com/cosmos/cosmos-sdk/cosmovisor/cmd/cosmovisor@v1.0.0
 mkdir -p $lavad_home_folder/cosmovisor
 wget https://lava-binary-upgrades.s3.amazonaws.com/testnet/cosmovisor-upgrades/cosmovisor-upgrades.zip
 unzip cosmovisor-upgrades.zip
 cp -r cosmovisor-upgrades/* $lavad_home_folder/cosmovisor
 
-# Set the environment variables
+   echo "Set the environment variables..."
 echo "# Setup Cosmovisor" >> ~/.profile
 echo "export DAEMON_NAME=lavad" >> ~/.profile
 echo "export CHAIN_ID=lava-testnet-1" >> ~/.profile
@@ -65,15 +58,13 @@ echo "export DAEMON_RESTART_AFTER_UPGRADE=true" >> ~/.profile
 echo "export UNSAFE_SKIP_BACKUP=true" >> ~/.profile
 source ~/.profile
 
-
-# Initialize the chain
+echo "Initialize the chain..."
 $lavad_home_folder/cosmovisor/genesis/bin/lavad init \
 $NODENAME \
 --chain-id lava-testnet-1 \
 --home $lavad_home_folder \
 --overwrite
 cp genesis_json/genesis.json $lava_config_folder/genesis.json
-
 
 # Create Cosmovisor unit file
 echo "[Unit]
@@ -98,16 +89,153 @@ WantedBy=multi-user.target
 sudo mv cosmovisor.service /lib/systemd/system/cosmovisor.service
 
 
+  
+}
 
-# Enable the cosmovisor service so that it will start automatically when the system boots
+function check_logs {
 
-sudo systemctl daemon-reload
-sudo systemctl enable cosmovisor.service
-sudo systemctl restart systemd-journald
-sudo systemctl start cosmovisor   
+sudo journalctl -u cosmovisor -f
+
+}
+
+function create_wallet {
+    echo "Creating your wallet.."
+    sleep 2
+    
+    current_lavad_binary="$HOME/.lava/cosmovisor/current/bin/lavad"
+    WALLET="WALLET"
+    $current_lavad_binary keys add $WALLET
+
+    
+    sleep 3
+    echo "DANGER!!!Ensure you write down the mnemonic as you can not recover the wallet without it."
 
 
-# Prepare an account
-current_lavad_binary="$HOME/.lava/cosmovisor/current/bin/lavad"
-ACCOUNT_NAME=wallet
-$current_lavad_binary keys add $ACCOUNT_NAME
+}
+
+function create_validator {
+$current_lavad_binary tx staking create-validator \
+    --amount="100000ulava" \
+    --pubkey=$($current_lavad_binary tendermint show-validator --home "$HOME/.lava/") \
+    --moniker=$MONIKER  \
+    --chain-id=lava-testnet-1 \
+    --commission-rate="0.10" \
+    --commission-max-rate="0.20" \
+    --commission-max-change-rate="0.01" \
+    --min-self-delegation="1" \
+    --gas="auto" \
+    --from=$WALLET -y
+
+}
+
+
+
+
+
+function select_option {
+    
+    ESC=$( printf "\033")
+    cursor_blink_on()  { printf "$ESC[?25h"; }
+    cursor_blink_off() { printf "$ESC[?25l"; }
+    cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
+    print_option()     { printf "   $1 "; }
+    print_selected()   { printf "  $ESC[7m $1 $ESC[27m"; }
+    get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
+    key_input()        { read -s -n3 key 2>/dev/null >&2
+                         if [[ $key = $ESC[A ]]; then echo up;    fi
+                         if [[ $key = $ESC[B ]]; then echo down;  fi
+                         if [[ $key = ""     ]]; then echo enter; fi; }
+
+    for opt; do printf "\n"; done
+
+    # determine current screen position for overwriting the options
+    local lastrow=`get_cursor_row`
+    local startrow=$(($lastrow - $#))
+
+    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    cursor_blink_off
+
+    local selected=0
+    while true; do
+        local idx=0
+        for opt; do
+            cursor_to $(($startrow + $idx))
+            if [ $idx -eq $selected ]; then
+                print_selected "$opt"
+            else
+                print_option "$opt"
+            fi
+            ((idx++))
+        done
+
+        case `key_input` in
+            enter) break;;
+            up)    ((selected--));
+                   if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+            down)  ((selected++));
+                   if [ $selected -ge $# ]; then selected=0; fi;;
+        esac
+    done
+
+    cursor_to $lastrow
+    printf "\n"
+    cursor_blink_on
+
+    return $selected
+}
+
+function print_logo {
+   echo  "
+
+████████╗██╗   ██╗██████╗ ██╗ █████╗ ███╗   ██╗ ██████╗ █████╗  
+╚══██╔══╝██║   ██║██╔══██╗██║██╔══██╗████╗  ██║██╔════╝██╔══██╗
+   ██║   ██║   ██║██████╔╝██║███████║██╔██╗ ██║██║     ███████║
+   ██║   ██║   ██║██╔══██╗██║██╔══██║██║╚██╗██║██║     ██╔══██║
+   ██║   ╚██████╔╝██████╔╝██║██║  ██║██║ ╚████║╚██████╗██║  ██║
+   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝╚═╝  ╚═╝
+ "                                                                             
+                                                                                        
+}
+
+function main {
+    cd $HOME
+
+    print_logo
+
+    echo "Choose the command you want to use:"
+
+    options=(
+        "🛠 Install Lava Node"
+        "👀 Check Logs"
+        "🔑 Create wallet"
+        "💎 Be validator"
+        "🚨 Exit"
+    )
+
+    select_option "${options[@]}"
+    choice=$?
+    clear
+
+    case $choice in
+        0)
+            install_node
+            ;;
+        1)
+            check_logs
+            ;;
+        2)
+            create_wallet
+            ;;    
+        3)
+            create_validator
+            ;;
+        4)
+            exit 0
+         
+            ;;
+    esac
+
+    echo -e $DEFAULT
+}
+
+main
